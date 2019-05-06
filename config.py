bd2178a94449fa03bef89d2704707410460ab8af
Anh Quang <QUang1994@web.de>
2019-05-06 15:54:12 +0200

    integration of db complete

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-import os
-from app import app
 Debug = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI =  'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+SQLALCHEMY_DATABASE_URI =  'sqlite:///db.sqlite'
 # SQLALCHEMY_DATABASE_URI =  'sqlite:///:memory:'
