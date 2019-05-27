df929ca384e0fea53f4140477b1778896c94d38c
Anh Quang <QUang1994@web.de>
2019-05-27 15:27:31 +0200

    registry working

--- docs/config.py
+++ docs/config.py
@@ -1,5 +1,5 @@
 Debug = True
-Secret_Key = 'e3992c6689e61d9943cc55a9fbf2c0fc'
+SECRET_KEY = 'e3992c6689e61d9943cc55a9fbf2c0fc'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_DATABASE_URI =  'sqlite:///db.sqlite'
 # SQLALCHEMY_DATABASE_URI =  'sqlite:///:memory:'
--- docs/views.py
+++ docs/views.py
@@ -1,35 +1,34 @@
 from app import app, db, ma, bcrypt
-from flask import request, jsonify, render_template,redirect, flash
+from flask import request, jsonify, render_template, redirect, flash, url_for
 from models.BookModel import Book, BookSchema
 from models.UserModel import User, UserSchema
-# from forms import RegistrationForm, LoginForm
 
 # Bücher erstellen
 # Erstellen Book (Post)
 book_schema = BookSchema(strict=True)
 book_schemas = BookSchema(many=True, strict=True)
 user_schema = UserSchema(strict=True)
-user_schemas = UserSchema(many=True,strict = True)
+user_schemas = UserSchema(many=True, strict=True)
 
 # buch adden
 @app.route('/book', methods=['POST'])
-def add_Book():
+def createBook():
     name = request.json['name']
     author = request.json['author']
     description = request.json['description']
     price = request.json['price']
     user_id = request.json['user_id']
 
-    new_Book = Book(name, author, description, price,user_id)
+    new_Book = Book(name, author, description, price, user_id)
     db.session.add(new_Book)
     db.session.commit()
     return book_schema.jsonify(new_Book)
 
 # Startseite mit allen Bücher werden angezeigt
-@app.route('/', methods=['GET','POST'])
-def get_Startpage():
+@app.route('/', methods=['GET'])
+def renderStartpage():
     all_books = Book.query.all()
-    return render_template('startpage.html', books = all_books)
+    return render_template('startpage.html', books=all_books)
 
 # getBook durch <id> (query parameter)
 @app.route('/book/<id>', methods=['GET'])
@@ -39,19 +38,26 @@ def get_Book_by_id(id):
 
 # update(put request) an sich genau so wie add, nur anders
 @app.route('/book/<id>', methods=['PUT'])
-def update_Book(id):
+def editBook(id):
     updated_book = Book.query.get(id)
     name = request.json['name']
     author = request.json['author']
     description = request.json['description']
     price = request.json['price']
-    
+
     updated_book.name = name
     updated_book.price = price
     updated_book.description = description
     updated_book.author = author
     db.session.commit()
     return book_schema.jsonify(updated_book)
+
+
+@app.route('/getbooks', methods=['GET'])
+def getAllBooks():
+    allbooks = Book.query.all()
+    result = book_schemas.dump(allbooks)
+    return jsonify(result.data)
 
 
 @app.route('/book/<id>', methods=['DELETE'])
@@ -63,39 +69,52 @@ def delete_book(id):
     return book_schema.jsonify(book_to_delete)
 
 # Users
-#addUser/registrate
-@app.route('/registration', methods=['GET','POST'])
+# addUser/registrate
+# 
+@app.route('/', methods=['GET', 'POST'])
 def registrate():
-    # name = request.json['name']
-    # password = request.json['password']
-    # email = request.json['email']
-    # userToAdd = User(name, password, email)
-    # db.session.add(userToAdd)
-    # db.session.commit()
-    # return user_schema.jsonify(userToAdd)
-    hashed_password = bcrypt.generate_password_hash(request.form['accountPassword']).decode('utf-8')
+    hashed_password = bcrypt.generate_password_hash(  # password encryption
+        request.form['accountPassword']).decode('utf-8')
     accountUsername = request.form['accountUsername']
     accountEmail = request.form['accountEmail']
-    user = User(name=accountUsername, email = accountEmail, password = hashed_password)
-    db.session.add(user)
+    user = User(name=accountUsername, email=accountEmail,  # generate user
+                password=hashed_password)
+
+    emailCheck = User.query.filter_by(email=accountEmail).first()
+    userNameCheck = User.query.filter_by(name=accountUsername).first()
+    if(emailCheck and userNameCheck):
+        flash('he already in nigger')
+        return redirect(url_for('renderStartpage'))
+
+    db.session.add(user)  # add user to db
     db.session.commit()
-    if(db.user.filter(db.user.name == user.name)):
-        flash("user eingefügt")
     return render_template('home.html')
-#get userbyid
-@app.route('/user/<id>',methods=['GET'])
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    inputEmail = request.form['loginEmail']
+    inputPassword = request.form['loginPassword']
+    user = User.query.filter_by(email=inputEmail).first()
+    if(user and bcrypt.check_password_hash(user), inputPassword):
+        return redirect(url_for('startpage'))
+    flash('Login no gut')
+    return render_template('home.html')
+
+# get userbyid
+@app.route('/user/<id>', methods=['GET'])
 def get_User_by_id(id):
     userToGet = User.query.get(id)
     return user_schema.jsonify(userToGet)
 
-#Getallusers
+# Getallusers
 @app.route('/user', methods=['GET'])
 def get_Users():
     all_Users = User.query.all()
     result = user_schemas.dump(all_Users)
     return jsonify(result.data)
 
-#delete
+# delete
 @app.route('/user/<id>', methods=['DELETE'])
 def deleteUser(id):
     userToDelete = User.query.get(id)
@@ -103,9 +122,10 @@ def deleteUser(id):
     db.session.commit()
     return user_schema.jsonify(userToDelete)
 
+
 @app.route('/user/<id>', methods=['PUT'])
 def update_user(id):
-    user_to_update = User.query.get(id) 
+    user_to_update = User.query.get(id)
     name = request.json['name']
     password = request.json['password']
     email = request.json['email']
