d1469e00338ba5bba523850c03cc0021f97a6fc7
Tim-Niklas <44227439+tim-niklas@users.noreply.github.com>
2019-06-03 16:17:52 +0200

    Add search with React

--- docs/static/react/startpage_components/footer.js
+++ docs/static/react/startpage_components/footer.js
@@ -2,14 +2,14 @@ class Footer extends React.Component {
   render(){
     return(
       <div>
-         <footer className="page-footer font-small bg-light">
-        <div className="text-center py-3 color-black">
-          <a className="m-1" href="#">Privacy Policy</a>
-          |
-          <a className="m-1" href="#">Legal Notice</a>
-          © 2019
-          <a className="m-1" href="#">Book Road</a>
-        </div>
+      <footer className="page-footer font-small bg-light">
+          <div className="text-center py-3 color-black">
+              <a className="m-1" href="#">Privacy Policy</a>
+              |
+              <a className="m-1" href="#">Legal Notice</a>
+              © 2019
+              <a className="m-1" href="#">Book Road</a>
+          </div>
       </footer>
       </div>
     )
--- docs/static/react/startpage_components/main.js
+++ docs/static/react/startpage_components/main.js
@@ -1,36 +1,86 @@
 class Main extends React.Component {
-  render(){
 
-    let row = [];
-    for (var i = 0; i < arrayBooksName.length; i++) {
-        row.push(
-        <div className="col-12 col-md-4 p-3 book-box">
-          <div className="bg-light"> 
-            <h1 className="ml-2 book-title">{arrayBooksName[i]}</h1>
-            <img className="float-left" src="../static/assets/images/books.png" alt="placeholder-image" width={100} height={100} />
-            <div className="overflow-hidden color-black p-0 box">
-              <p className="ml-2 my-1 font-italic">{arrayBooksAuthor[i]}</p>
-              <p className="ml-2 my-1 font-weight-bold">Price: {arrayBooksPrice[i]} €</p>
-              <p><a className="ml-2 my-1 color-black" tabIndex="0" data-toggle="popover" data-trigger="focus" data-placement="bottom" title="Description" data-content="halo">Description ▼</a></p>
-              <div className="email">
-                <a className="h1" href={'mailto:' + arrayBooksEmail[i] + '?subject=Your Advert:' + arrayBooksName[i]} >✉</a>
+    state = {
+        search: ""
+    };
+      
+    renderBooks= books => {
+        const { search } = this.state;
+    return (
+        <div className="col-12 col-md-4 p-3 book-box" key={books.id}>
+            <div className="bg-light">
+                <h1 className="ml-2 book-title">{books.name}</h1>
+                <img className="float-left" src="../static/assets/images/books.png" alt="Book" width={100}
+                    height={100} />
+                <div className="overflow-hidden color-black p-0 box">
+                    <p className="ml-2 my-1 font-italic">{books.author}</p>
+                    <p className="ml-2 my-1 font-weight-bold">Price: {books.price} €</p>
+                    <p><a className="ml-2 my-1 color-black" tabIndex="0" data-toggle="popover" data-trigger="focus"
+                            data-placement="bottom" title="Description" data-content={books.description}>Description ▼</a>
+                    </p>
+                    <div className="email">
+                        <a className="h1" href={'mailto:' + books.email + '?subject=Your Advert:' + books.name}>✉</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      );
+    };
+
+    onchange = e => {
+        this.setState({ search: e.target.value });
+    };
+
+    render() {
+        const { search } = this.state;
+        const filteredBooks = booksArray.filter(books => {
+        return books.name.toLowerCase().indexOf(search.toLowerCase()) !== -1;
+    });
+   
+    return (
+    <div>
+      <header>
+          <nav className="navbar navbar-default fixed-top">
+              <div className="container-fluid">
+                  <div className="navbar-header">
+                      <a className="navbar-brand" href="#">
+                          <img src="../static/assets/images/logo_nav.png" className="rounded-circle" alt="Logo"
+                              width={40} height={40} />
+                          <span className="ml-1">Book Road</span>
+                      </a>
+                  </div>
+                  <ul className="nav navbar-nav">
+                      <form className="form-inline my-2 my-lg-0">
+                          <div className="input-group md-form form-sm form-2 pl-0">
+                              <input id="search-bar" className="form-control my-0 py-1 shadow-none" type="text"
+                                  name="search" placeholder="Search title .." aria-label="Search title" onChange={this.onchange}/>
+                              <div className="input-group-append">
+                                  <span className="input-group-text red lighten-3" id="basic-text1"><i
+                                          className="fa fa-search text-grey" aria-hidden="true"/></span>
+                              </div>
+                          </div>
+                      </form>
+                  </ul>
+                  <ul className="nav navbar-nav navbar-right">
+                      <button type="button" className="btn btn-primary shadow-none" data-toggle="modal"
+                          data-target="#exampleModalCenter">
+                          Sign in
+                      </button>
+                  </ul>
               </div>
+          </nav>
+      </header>
+
+      <main>
+      <div className="container-fluid">
+          <div className="row">
+              {filteredBooks.map(books => {
+                return this.renderBooks(books);
+              })}
             </div>
           </div>
-        </div>
-        );
-    }
-
-    return(
-      <div>
-         <main>
-         <div className="container-fluid">
-            <div className="row">
-  	        {row} 
-          </div>
-        </div>
       </main>
-      </div>
+    </div>
     )
   }
 }
