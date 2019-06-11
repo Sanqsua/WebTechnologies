86a2cb45d218cb14abef0bff25376331198474ab
Tim-Niklas <44227439+tim-niklas@users.noreply.github.com>
2019-06-11 16:02:21 +0200

    Add Home Button

--- docs/static/react/startpage_components/login.js
+++ docs/static/react/startpage_components/login.js
@@ -8,7 +8,7 @@ class Login extends React.Component {
         <div className="modal fade" id="exampleModalCenter" tabIndex={-1} role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
           <div className="modal-dialog modal-dialog-centered" role="document">
             <div className="modal-content text-center">              
-              <form className="form-signin" method="GET">
+              <form className="form-signin" action="/login" method="POST">
                 <img className="mb-2" src="../static/assets/images/login_logo.png" alt="Logo" width="150" height="150" />
                 <h1 className="h3 mb-3 font-weight-normal">Please sign in</h1>
                 <label htmlFor="inputEmail" className="sr-only">Email address</label>
