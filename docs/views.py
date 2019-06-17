80ad7de5e933d891f1b35bbaefc9821fc66beedc
Anh Quang <QUang1994@web.de>
2019-06-17 15:06:30 +0200

    updates now function properly

--- docs/views.py
+++ docs/views.py
@@ -174,22 +174,27 @@ def editUser():
         abort(403)
     # Inputs
     newUsername = request.form['editUsername']
+
     email = request.form['editEmail']
-    password = bcrypt.generate_password_hash(
-        request.form['editNewPassword']).decode('utf-8')
+    password = None
+    if request.form['editNewPassword']:
+        password = bcrypt.generate_password_hash(
+            request.form['editNewPassword']).decode('utf-8')
+
     current_Password = request.form['editCurrentPassword']
-#Look if strings is not empty
+# Passwordcheck and User not in database check
+    currentPasswordCheck = bcrypt.check_password_hash(
+        user_to_update.password, current_Password)
+    usernameNotInDataBase = not User.query.filter_by(name=newUsername).first()
+# Look if strings is not empty
+
     if newUsername:
         user_to_update.name = newUsername
     if email:
         user_to_update.email = email
     if password:
         user_to_update.password = password
-        
-# Passwordcheck and User not in database check
-    currentPasswordCheck = bcrypt.check_password_hash(
-        user_to_update.password, current_Password)
-    usernameNotInDataBase = not User.query.filter_by(name=newUsername).first()
+
     if(currentPasswordCheck and usernameNotInDataBase):
         user_to_update.password = password
         db.session.commit()
